767efd1e417e70fddcd3ecafda8353fe28f9682b
valentin994 <valentin.vareskic@gmail.com>
2026-06-02 12:39:42 +0200

    finished question 26

--- 26_remove_duplicate_from_sorted_array/task.py
+++ 26_remove_duplicate_from_sorted_array/task.py
@@ -1,27 +1,15 @@
 class Solution:
     def removeDuplicates(self, nums: list[int]) -> int:
-        pop_counter = 0
-        if len(nums) <= 1:
-            return 1
-        for i in range(len(nums)):
-            if i == 0:
-                continue
-            cursor = nums[i - 1]
-            if cursor != nums[i]:
-                pop_counter += 1
-                nums[pop_counter] = nums[i]
-        if nums[pop_counter + 1] != nums[len(nums) - 1]:
-            nums[pop_counter + 1] = nums[len(nums) - 1]
-            pop_counter += 1
-        if pop_counter == 0 and len(nums) == 2:
-            nums[1] = -1
-        if pop_counter == 0:
-            return len(nums)
-        return pop_counter + 1
+        curr = 0
+        for i in range(1, len(nums)):
+            if nums[i] != nums[curr]:
+                curr += 1
+                nums[curr] = nums[i]
+        return curr + 1
 
 
-nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
-# nums = [1, 1]
+# nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+nums = [1, 2, 2]
 s = Solution()
 
 print(s.removeDuplicates(nums))
